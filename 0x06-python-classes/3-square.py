390e9831a182be6d66f6b03869b01b6650adae71
ghazalasfr <ghazal.lsfr@gmail.com>
2023-11-21 12:08:51 -0800

    task 4

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
+"""
+Module: 3-square
+Class Square that defines a square by:
+- Private instance attribute: size
+- Instantiation with optional size: def __init__(self, size=0)
+- Size must be an integer, otherwise raise a TypeError
+  exception with the message size must be an integer
+- If size is less than 0, raise a ValueError exception with
+  the message size must be >= 0
+- Public instance method: def area(self) that returns the current square area
+"""
+
+
 class Square:
-    """Square Class
-
-    A Square Class
-
-    """
+    """Represents a square"""
 
     def __init__(self, size=0):
-        """__init__
+        """Initialize a new square.
 
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
+        Args:
+            size (int, optional): The size of the square,
+                    Defaults to 0
         """
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
     def area(self):
-        """Returns the current square area
+        """Calculates the area of square
 
+        Returns:
+            int: The area of the square.
         """
         return self.__size ** 2
 
