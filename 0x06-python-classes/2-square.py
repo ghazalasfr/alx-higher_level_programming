7ac50c30aab09f1df66d0ce00b78d2dbfb715bac
ghazalasfr <ghazal.lsfr@gmail.com>
2023-11-21 12:07:15 -0800

    task 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""Square Class
-
-A Square Class
-
+"""
+Module: 2
+- Size must be an integer, otherwise raise a TypeError
+  exception with the message size must be an integer
+- If size is less than 0, raise a ValueError
+  exception with the message size must be >= 0
 """
 
 
 class Square:
+    """Represents a square"""
 
     def __init__(self, size=0):
-        """__init__
+        """Initialize a new square.
 
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
+        Args:
+            size (int, optional): The size of the square,
+                    Defaults to 0
         """
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
