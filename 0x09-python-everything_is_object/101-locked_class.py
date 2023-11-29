19a6911bdca0c3247cff775e5bbfe223c954d899
ghazalasfr <ghazal.lsfr@gmail.com>
2023-11-29 10:28:27 -0800

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,8 +4,12 @@
 A module with with no class or object attribute
 
 """
+
 class LockedClass(object):
     '''
-     class LockedClass with no class or object attribute
+    
+    class LockedClass with no class or object attribute
+
     '''
+
     __slots__ = 'first_name'
