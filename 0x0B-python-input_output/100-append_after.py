5f5cdc7813b82bf0e6a73f4049a4c032a0344d37
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-05 13:37:09 -0800

    stats

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+-advanced modul 
+-append after
+"""
+
+def append_after(filename="", search_string="", new_string=""):
+    """append after function that open file and add item"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        text = f.readlines()
+        new_text = []
+
+        for line in text:
+            new_text.append(line)
+
+            if search_string in line:
+                new_text.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as nf:
+        for line in new_text:
+            nf.write(line)
