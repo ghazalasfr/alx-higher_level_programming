1771fdb1b1032dacd8e7fcd3119013aa0d25e407
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-05 12:42:18 -0800

    fixed

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+A Checks if `obj` is exactly an instance of the specified class
+"""
+
 def is_same_class(obj, a_class):
     """
     Checks if `obj` is exactly an instance of the specified class
