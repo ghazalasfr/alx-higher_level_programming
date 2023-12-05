b79e2bb11a6f9288ba7eabf389badd6b4a25d2ea
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-05 13:04:35 -0800

    all files

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module - 6
+- Creates object from JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Loads JSON from file"""
+    with open(filename, "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+    return data
