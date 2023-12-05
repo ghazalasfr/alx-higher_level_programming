295ce8434a17149a4933b69efcefdc223fdaf7cd
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-05 12:40:34 -0800

    fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+
 """
 A module to prints a list in ascending order
 """
 
 def lookup(obj):
-
     """
     Returns the list of available attributes and methods of an object
     """
