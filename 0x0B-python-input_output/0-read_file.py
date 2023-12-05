30a3bf23ec07330024c204783df6732da86348c3
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-05 12:52:59 -0800

    doc

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout:
+"""
+
 def read_file(filename=""):
     """
     Write a function that reads a text file (UTF8) and prints it to stdout:
     """
 
     with open(filename, encoding='utf-8') as f:
+
+        """
+        function open to open the file 
+        """
+
         for line in f:
             print(line, end='')
