645e62660c63ba1024bf85b5f297df644980a0ca
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-06 12:02:52 -0800

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
 Module - 7
-- import moduls 
-- add item
+- Add items and save them in a file
 """
+import sys
 
-from os import path
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
 
-save_to_json_file(obj_json_file, 'add_item.json')
+if __name__ == '__main__':
+    try:
+        item_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item_list = []
+
+    item_list.extend(sys.argv[1:])
+
+    save_to_json_file(item_list, "add_item.json")
