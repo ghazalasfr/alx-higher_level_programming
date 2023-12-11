2fdff799029c7ac4b8d39b7722222d4cfb2cde90
ghazalasfr <ghazal.lsfr@gmail.com>
2023-12-11 13:35:47 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-the rectangle class with all function
+the rectangle class that ihrets from base
 """
 
 from models.base import Base
@@ -8,19 +8,19 @@ from models.base import Base
 
 class Rectangle(Base):
     """
-    rectangle class
+    the main class of rectangle 
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        the constructor of rectangle class
+        the init / constructor of the rectangle class
         """
         super().__init__(id)
 
-        self.check_function(width, 'width')
-        self.check_function(height, 'height')
-        self.check_function(x, 'x')
-        self.check_function(y, 'y')
+        self.check_integer(width, 'width')
+        self.check_integer(height, 'height')
+        self.check_integer(x, 'x')
+        self.check_integer(y, 'y')
 
         self.__width = width
         self.__height = height
@@ -30,75 +30,134 @@ class Rectangle(Base):
     @property
     def width(self):
         """
-        width function that return width
+        witdh of the rectangle
         """
         return self.__width
 
     @width.setter
-    def width(self, newval):
+    def width(self, val):
         """
-        setter of width that set the width to new value
+        the setter of width
         """
-        self.check_function(newval, 'width')
+        self.check_integer(val, 'width')
 
-        self.__width = newval
+        self.__width = val
 
     @property
     def height(self):
         """
-        heisght function
+        the height of the rectangle
         """
         return self.__height
 
     @height.setter
-    def height(self, newval):
+    def height(self, val):
         """
         setter of height
         """
-        self.check_function(newval, 'height')
+        self.check_integer(val, 'height')
 
-        self.__height = newval
+        self.__height = val
 
     @property
     def x(self):
         """
-        the x parameter
+        property x
         """
         return self.__x
 
     @x.setter
-    def x(self, newX):
+    def x(self, val):
         """
-        setter of x 
+        setter of x
         """
-        self.check_function(newX, 'x')
+        self.check_integer(val, 'x')
 
-        self.__x = newX
+        self.__x = val
 
     @property
     def y(self):
         """
-        the y parameter
+        property y
         """
         return self.__y
 
     @y.setter
-    def y(self, newY):
+    def y(self, val):
         """
         setter of y
         """
-        self.check_function(newY, 'y')
+        self.check_integer(val, 'y')
 
-        self.__y = param
- def check_function(self, value, newValue):
+        self.__y = val
+
+    def check_integer(self, value, val):
         """
-        ...
+        the function that check if the value is integer or not
         """
         if type(value) is not int:
-            raise TypeError(newValue + ' must be an integer')
+            raise TypeError(val + ' must be an integer')
 
-        if value <= 0 and newValue in ('width', 'height'):
-            raise ValueError(newValue + ' must be > 0')
+        if value <= 0 and val in ('width', 'height'):
+            raise ValueError(val + ' must be > 0')
 
-        if value < 0 and newValue in ('x', 'y'):
-            raise ValueError(newValue + ' must be >= 0')
+        if value < 0 and val in ('x', 'y'):
+            raise ValueError(val + ' must be >= 0')
+
+    def area(self):
+        """
+        the area function that return with*height 
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        display function that print the result
+        """
+        if self.__y > 0:
+            print('\n' * self.__y, end='')
+
+        for i in range(self.height):
+            if self.__x > 0:
+                print(' ' * self.__x, end='')
+
+            print('#' * self.__width)
+
+    def __str__(self):
+        """
+        the str function
+        """
+        return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
+            self.id, self.x, self.y, self.width, self.height
+        )
+
+    def update(self, *args, **kwargs):
+        """
+        the update function that make updates of all args
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'width', 'height', 'x', 'y']
+
+        if argc > 5:
+            argc = 5
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
+
+    def to_dictionary(self):
+        """
+        to dictionary function
+        """
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
