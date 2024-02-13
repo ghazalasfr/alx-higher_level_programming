a69d4aeb982f4d8788f5299af4c655c4953d37bb
ghazalasfr <ghazal.lsfr@gmail.com>
2024-02-13 11:55:08 -0800

    update

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// a Square class that extends Rectangle class
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+};
+
